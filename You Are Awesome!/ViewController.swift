44fe91e5b36d4088896d4259c170af3ad0423c71
Michael Ross <viewsub@gmail.com>
2022-02-23 23:34:05 -0500

    2.10 added message arrays

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,15 +11,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    var messageNumber = 0
+    
+      
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
     }
 
-
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "Nice!"
-        imageView.image = UIImage(named: "image1")
+        
+        let messages = ["Nice!",
+                        "Yeah!",
+                        "Woohoo!",
+                        "Great!",
+                        "Cool!",
+                        "Awesome!"]
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        
+        
+        print (imageNumber)
+        //  let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+        
+        
+        //        let niceMessage = "Nice!"
+        //        let wowMessage = "Wow!"
+        //        let coolMessage = "Cool!"
+        //
+        //        if messageLabel.text == niceMessage {
+        //            messageLabel.text = wowMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        } else if messageLabel.text == wowMessage {
+        //            messageLabel.text = coolMessage
+        //            imageView.image = UIImage(named: "image5")
+        //        } else {
+        //            messageLabel.text = niceMessage
+        //            imageView.image = UIImage(named: "image4")
+        //        }
+        
     }
 }
 
